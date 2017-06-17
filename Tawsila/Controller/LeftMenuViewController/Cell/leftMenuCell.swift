8df11e2b2f1f11d4acddf9192931cc8202ab39f2
aniljangir0907 <aniljangir@foxity.in>
2017-06-17 17:54:11 +0530

    first commit

---
+++ Tawsila/Controller/LeftMenuViewController/Cell/leftMenuCell.swift
@@ -0,0 +1,24 @@
+//
+//  leftMenuCell.swift
+//  Tawsila
+//
+//  Created by vikram singh charan on 6/14/17.
+//  Copyright © 2017 scientificweb. All rights reserved.
+//
+
+import UIKit
+
+class leftMenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Tawsila/Controller/VSCSliderViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  VSCSliderViewCell.swift
+//  Tawsila
+//
+//  Created by vikram singh charan on 6/11/17.
+//  Copyright © 2017 scientificweb. All rights reserved.
+//
+
+import UIKit
+
+class VSCSliderViewCell: UITableViewCell {
+
+    @IBOutlet var imgIcon: UIImageView!
+    @IBOutlet var lblTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
